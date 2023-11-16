20cb2e8d982be73c7c0af74efd675b746783ea42
Joonas Timlin <joonas@timlins.se>
2023-11-16 09:37:56 +0000

    setstates

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import StatefulGreetingWithPrevState from "./components/StatefulGreetingWIthPrevState";
+
+function App() {
+  return (
+    <div className="App">
+      <StatefulGreetingWithPrevState />
+    </div>
+  );
+}
+
+export default App;
